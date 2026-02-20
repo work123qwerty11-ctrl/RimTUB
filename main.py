0fa696731352aff50690e760aea1516405256595
work123qwerty11-ctrl <work123qwerty11@gmail.com>
2026-02-20 11:50:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import asyncio
 import logging, coloredlogs
-# logging.SPAM = 6
+
+# --- ИСПРАВЛЕНИЕ ДЛЯ PYTHON 3.14 (Добавь это первым делом) ---
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+# ----------------------------------------------------------
+
 logger = logging.getLogger('RimTUB')
 from config import LOGGING_LEVEL
 logger.setLevel(LOGGING_LEVEL)
 coloredlogs.install(logger=logger, level=logger.level,
     fmt='%(asctime)s %(name)s %(levelname)s: %(message)s'
 )
-
-# logging.log(logging.SPAM, 'sfdf')
-
-if __name__ == '__main__':
-    logger.info("Запускаюсь...")
 
 from pyrogram import idle
 from pyrogram.enums.parse_mode import ParseMode
@@ -20,17 +23,18 @@ from config import API_ID, API_HASH, PHONES, PLAY_SOUND, BOT_TOKEN
 from utils import get_script_directory, ModifyPyrogramClient
 
 from sys import argv
-
- 
 from telebot.async_telebot import AsyncTeleBot 
- 
-version = '1.7'
 
+version = '1.7'
 clients = []
 
 def start():
     bot = AsyncTeleBot(BOT_TOKEN, 'html', colorful_logs=True)
-    asyncio.get_event_loop().create_task(bot.polling(none_stop=True))
+    
+    # Исправленный запуск опроса бота
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.polling(none_stop=True))
+    
     for i, PHONE in enumerate(PHONES):
         account_logger = logging.getLogger(f'RimTUB [{i}]')
         account_logger.setLevel(LOGGING_LEVEL)
@@ -68,25 +72,20 @@ def start():
         clients.append(cl)
 
     if (True if len(argv) <= 1 else False) if PLAY_SOUND else False:
-        
         from os import environ
         environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-
         from pygame.mixer import Sound, init
-
-        init()
-
-        Sound('started.mp3').play()
-
+        try:
+            init()
+            Sound('started.mp3').play()
+        except:
+            pass
 
     logger.info("\n\n- RimTUB Запущен и готов к работе! -\n")
-
-
     idle()
     for cl in clients:
-        # cl: ModifyPyrogramClient
         cl.stop()
 
-
 if __name__ == '__main__':
+    logger.info("Запускаюсь...")
     start()
